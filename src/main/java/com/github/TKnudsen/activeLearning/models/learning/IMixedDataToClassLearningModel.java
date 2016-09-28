e9b9d53debabdfb8b946cd6941ed08f58f8946f6
jubernar <juergen.bernard@igd.fraunhofer.de>
2016-09-28 20:14:45 +0200

    Interfaces for MixedData ML

---
+++ src/main/java/com/github/TKnudsen/activeLearning/models/learning/IMixedDataToClassLearningModel.java
@@ -0,0 +1,7 @@
+package com.github.TKnudsen.activeLearning.models.learning;
+
+import com.github.TKnudsen.ComplexDataObject.data.features.mixedData.MixedDataFeatureVector;
+
+public interface IMixedDataToClassLearningModel extends ILearningModel<Object, MixedDataFeatureVector, String> {
+
+}
