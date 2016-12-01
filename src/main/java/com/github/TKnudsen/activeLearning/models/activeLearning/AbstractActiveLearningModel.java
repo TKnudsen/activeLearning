edc4d03392b16f48e871435ceee7d8277375d71a
Christian Ritter <christian.ritter.alz@web.de>
2016-12-01 19:38:13 +0100

    new active learning model created ranking in AbstractActiveLearningModel now has a getter

--- src/main/java/com/github/TKnudsen/activeLearning/models/activeLearning/AbstractActiveLearningModel.java
+++ src/main/java/com/github/TKnudsen/activeLearning/models/activeLearning/AbstractActiveLearningModel.java
@@ -1,62 +1,63 @@
 package com.github.TKnudsen.activeLearning.models.activeLearning;
 
-import java.util.ArrayList;
-import java.util.List;
-
 import com.github.TKnudsen.ComplexDataObject.data.entry.EntryWithComparableKey;
 import com.github.TKnudsen.ComplexDataObject.data.features.numericalData.NumericalFeatureVector;
 import com.github.TKnudsen.ComplexDataObject.data.ranking.Ranking;
 import com.github.TKnudsen.activeLearning.models.learning.ILearningModel;
 import com.github.TKnudsen.activeLearning.models.learning.classification.IClassifier;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public abstract class AbstractActiveLearningModel implements IActiveLearningModelClassification<Double, NumericalFeatureVector> {
 
-	public AbstractActiveLearningModel(IClassifier<Double, NumericalFeatureVector> learningModel) {
-		this.learningModel = learningModel;
-	}
+    protected List<NumericalFeatureVector> trainingFeatureVectors;
+    protected List<NumericalFeatureVector> learningCandidateFeatureVectors;
+    protected Ranking<EntryWithComparableKey<Double, NumericalFeatureVector>> ranking;
+    protected Double remainingUncertainty;
+    protected IClassifier<Double, NumericalFeatureVector> learningModel;
+    public AbstractActiveLearningModel(IClassifier<Double, NumericalFeatureVector> learningModel) {
+        this.learningModel = learningModel;
+    }
 
-	protected List<NumericalFeatureVector> trainingFeatureVectors;
-	protected List<NumericalFeatureVector> learningCandidateFeatureVectors;
+    public Ranking<EntryWithComparableKey<Double, NumericalFeatureVector>> getRanking() {
+        return ranking;
+    }
 
-	protected Ranking<EntryWithComparableKey<Double, NumericalFeatureVector>> ranking;
-	protected Double remainingUncertainty;
+    @Override
+    public void setTrainingData(List<NumericalFeatureVector> featureVectors) {
+        this.trainingFeatureVectors = featureVectors;
+    }
 
-	protected IClassifier<Double, NumericalFeatureVector> learningModel;
+    @Override
+    public void setLearningCandidates(List<NumericalFeatureVector> featureVectors) {
+        this.learningCandidateFeatureVectors = featureVectors;
 
-	@Override
-	public List<NumericalFeatureVector> suggestCandidates(int count) {
+        ranking = null;
+    }
 
-		if (ranking == null)
-			calculateRanking(count);
+    @Override
+    public List<NumericalFeatureVector> suggestCandidates(int count) {
 
-		List<NumericalFeatureVector> fvs = new ArrayList<>();
-		for (int i = 0; i < ranking.size(); i++)
-			fvs.add(i, ranking.get(i).getValue());
+        if (ranking == null)
+            calculateRanking(count);
 
-		return fvs;
-	}
+        List<NumericalFeatureVector> fvs = new ArrayList<>();
+        for (int i = 0; i < ranking.size(); i++)
+            fvs.add(i, ranking.get(i).getValue());
 
-	protected abstract void calculateRanking(int count);
+        return fvs;
+    }
 
-	@Override
-	public void setTrainingData(List<NumericalFeatureVector> featureVectors) {
-		this.trainingFeatureVectors = featureVectors;
-	}
+    protected abstract void calculateRanking(int count);
 
-	@Override
-	public void setLearningCandidates(List<NumericalFeatureVector> featureVectors) {
-		this.learningCandidateFeatureVectors = featureVectors;
+    @Override
+    public double getRemainingUncertainty() {
+        return remainingUncertainty;
+    }
 
-		ranking = null;
-	}
-
-	@Override
-	public double getRemainingUncertainty() {
-		return remainingUncertainty;
-	}
-
-	@Override
-	public ILearningModel<Double, NumericalFeatureVector, String> getLearningModel() {
-		return learningModel;
-	}
+    @Override
+    public ILearningModel<Double, NumericalFeatureVector, String> getLearningModel() {
+        return learningModel;
+    }
 }
